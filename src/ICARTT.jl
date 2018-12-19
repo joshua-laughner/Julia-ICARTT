0ae37c024842768702bcef13924f852be29aa740
Joshua Laughner <jlaugh@caltech.edu>
2018-12-19 11:32:28 -0800

    Export the most commonly used functions from the top module

--- src/ICARTT.jl
+++ src/ICARTT.jl
@@ -5,5 +5,8 @@ include("units_icartt.jl")
 include("read_icartt.jl")
 include("user_utils.jl")
 
+import .ReadICARTT: read_icartt_file;
+import .ICARTTUtils: get_merge_data;
+export read_icartt_file, get_merge_data;
 
 end # module
